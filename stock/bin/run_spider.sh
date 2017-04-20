c3330b986018e81cfd2aff058ed24afeb0436415
lyx107131 <lyx107131@alibaba-inc.com>
2017-04-20 14:47:12 +0800

    modify

--- stock/bin/run_spider.sh
+++ stock/bin/run_spider.sh
@@ -5,11 +5,7 @@
 # Date: 2017-04-17
 # Description: 启动爬虫程序
 
-current_date=`date +%Y%m%d`
-echo $current_date
-PYTHON3 = /Library/Frameworks/Python.framework/Versions/3.6/bin:/usr/local/bin
-PATH=$PATH:PYTHON3
-echo $PATH
+current_date=`date +%Y%m%d%H`
 echo `python3 --version`
 cd /Users/leungyuxin/PycharmProjects/Machine-Learning/stock/src/download/
-nohup python3 -u main.py > ../../logs/current_date.log&
+nohup python3 -u main.py > ../../logs/${current_date}.log&
